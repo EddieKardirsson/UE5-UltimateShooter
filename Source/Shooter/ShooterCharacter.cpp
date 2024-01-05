ddbf39e4e90575157253683e10c3ad0fa50c1193
Edison Kardirsson <ed.kardirsson@hotmail.com>
2024-01-05 17:08:59 +0100

    Shot Direction fixed

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -139,6 +139,68 @@ void AShooterCharacter::PrimaryAttack(const FInputActionValue& Value)
 
 		if(MuzzleFlash) UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), MuzzleFlash, SocketTransform);
 
+		// Get current size of the viewport
+		FVector2D ViewportSize;
+		if(GEngine && GEngine->GameViewport)
+		{
+			GEngine->GameViewport->GetViewportSize(ViewportSize);
+		}
+
+		// Get screen space location of crosshair
+		FVector2D CrosshairLocation(ViewportSize.X / 2.f, ViewportSize.Y / 2.f);
+		CrosshairLocation.Y -= 50.f;
+		FVector CrosshairWorldPosition;
+		FVector CrosshairWorldDirection;
+
+		// Get World position and direction of crosshair
+		bool bScreenToWorld = UGameplayStatics::DeprojectScreenToWorld(
+			UGameplayStatics::GetPlayerController(this, 0),
+				CrosshairLocation,
+				CrosshairWorldPosition,
+				CrosshairWorldDirection
+		);
+
+		if(bScreenToWorld)	// was deprojection successful?
+		{
+			FHitResult ScreenTraceHit;
+			const FVector Start = CrosshairWorldPosition;
+			const FVector End = CrosshairWorldPosition + CrosshairWorldDirection * 50'000;
+
+			// Set beam end point to line trace endpoint
+			FVector BeamEndPoint = End;
+
+			// Trace outward from crosshair world location
+			GetWorld()->LineTraceSingleByChannel(
+				ScreenTraceHit,
+				Start, End,
+				ECollisionChannel::ECC_Visibility
+			);
+
+			if(ScreenTraceHit.bBlockingHit)		// was there a trace hit?
+			{
+				// Beam endpoint is now trace hit location
+				BeamEndPoint = ScreenTraceHit.Location;
+				if(ImpactParticles) UGameplayStatics::SpawnEmitterAtLocation(
+					GetWorld(),
+					ImpactParticles,
+					ScreenTraceHit.Location
+					);				
+			}
+			if(BeamParticles)
+			{
+				UParticleSystemComponent* Beam = UGameplayStatics::SpawnEmitterAtLocation(
+					GetWorld(),
+					BeamParticles,
+					SocketTransform
+				);
+				if(Beam)
+				{
+					Beam->SetVectorParameter(FName("Target"), BeamEndPoint);
+				}
+			}
+		}
+
+		/*
 		FHitResult PrimaryAttackHitResult;
 		const FVector Start = SocketTransform.GetLocation();
 		const FQuat Rotation = SocketTransform.GetRotation();
@@ -168,7 +230,7 @@ void AShooterCharacter::PrimaryAttack(const FInputActionValue& Value)
 			{
 				Beam->SetVectorParameter(FName("Target"), BeamEndPoint);
 			}
-		}
+		} */
 	}	
 	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
 	if(AnimInstance && PrimaryAttackMontage)
