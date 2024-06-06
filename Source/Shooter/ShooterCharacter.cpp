ff907e6459377e59749669a01410b73816cc5f28
Edison Kardirsson <ed.kardirsson@hotmail.com>
2024-06-06 22:10:01 +0200

    Throw Weapon

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -537,6 +537,7 @@ void AShooterCharacter::DropWeapon()
 		EquippedWeapon->GetItemMesh()->DetachFromComponent(DetachmentTransformRules);
 
 		EquippedWeapon->SetItemState(EItemState::EIS_Falling);
+		EquippedWeapon->ThrowWeapon();
 	}
 }
 
